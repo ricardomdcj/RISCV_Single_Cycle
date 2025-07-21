85646289833de4684bcb9b740aa466bcaec45505
brunofaus <brunofaus01@gmail.com>
2025-07-21 18:57:26 -0300

    Update instruction_memory.v

--- instruction_memory/instruction_memory.v
+++ instruction_memory/instruction_memory.v
@@ -3,7 +3,7 @@ module instruction_memory (
     output reg [31:0] RD //!Dados lidos da memória de instruções
 );
 
-    reg [31:0] memory [0:255]; //!Memória de instruções de 32 bits e 256 palavras
+    reg [31:0] memory [0:1 << 16]; //!Memória de instruções de 32 bits e 256 palavras
 
     initial begin
         // Inicializa a memória com algumas instruções de exemplo múltiplas de 4
